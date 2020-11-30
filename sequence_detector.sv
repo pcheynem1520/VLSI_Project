cef9a918f0851a8f860356e182f8626f02a4c29e
pcheynem1520 <pcheynem1520@gmail.com>
2020-11-29 22:55:23 -0500

    removed duplication

--- sequence_detector.sv
+++ sequence_detector.sv
@@ -49,83 +49,34 @@ module sequence_detector(
     // 100 -> null -> error, go to start
     // 101 -> null -> error, go to start
 
-    /* state switch logic */
-    always @(posedge clk) begin
-        if (rst) begin
-           state <= 3'b000;
-           count_detect <= 0;
-        end
-        else begin
-           state <= next_state; 
-           if (z) begin // not posedge in case of 2 consecutive detections
-               count_detect <= count_detect + 1;
-           end
-        end
-    end
-
-    /* next_state logic */
-    //always_comb begin
-        /* next-state swtich statement */
-        /*
-        case (state)
-            start:
-                if (sig_to_test) begin
-                    next_state = start;
-                end else begin
-                    next_state = first;
-                end
-            first:
-                if (sig_to_test) begin
-                    next_state = second;
-                end else begin
-                    next_state = first;
-                end
-            second:
-                if (sig_to_test) begin
-                    next_state = success;
-                end else begin
-                    next_state = delay;
-                end
-            delay:
-                if (sig_to_test) begin
-                    next_state = success_delay;
-                end else begin
-                    next_state = delay;
-                end
-            success_delay:
-                if (sig_to_test) begin
-                    next_state = success;
-                end else begin
-                    next_state = delay;
-                end
-            success:
-                if (sig_to_test) begin
-                    next_state = start;
-                end else begin
-                    next_state = first;
-                end
-            default: next_state = start;
-        endcase
-        */
-//    end
-
-    /* combinational next-state logic */
+    /* next-state logic */
     always_comb begin
-        next_state[2] <= (state[1] & state[0] & ~sig_to_test) | (state[2] & ~sig_to_test) | (state[2] & state[0]);
-        next_state[1] <= (state[0] & sig_to_test) | (state[1] & state[0]) | (state[2]);
-        next_state[1] <= (~sig_to_test) | (~state[1] & state[0]);
+        d_ff[2] <= (state[1] & state[0] & ~sig_to_test) | (state[2] & ~sig_to_test) | (state[2] & state[0]);
+        d_ff[1] <= (state[0] & sig_to_test) | (state[1] & state[0]) | (state[2]);
+        d_ff[1] <= (~sig_to_test) | (~state[1] & state[0]);
     end
 
     /* D flip-flops */
     always @(posedge clk) begin
-        state[2] <= next_state[2];
-        state[1] <= next_state[1];
-        state[0] <= next_state[0];
+        if (rst) begin
+           state[2] <= 1'b0;
+           state[1] <= 1'b0;
+           state[0] <= 1'b0;
+           count_detect <= 0;
+        end
+        else begin
+            state[2] <= next_state[2];
+            state[1] <= next_state[1];
+            state[0] <= next_state[0];
+            if (z) begin // not posedge in case of 2 consecutive detections
+               count_detect <= count_detect + 1;
+            end
+        end
     end
 
     /* output logic */
     always_comb begin
-        z <= next_state[1] & ~next_state[0];
+        z <= state[1] & ~state[0];
     end
 
     /* 7-segment display control logic */
@@ -134,7 +85,6 @@ module sequence_detector(
             disp0 = 7'b1000000;
             disp1 = 7'b1000000;
         end
-
         else if (ena) begin // enable signal high
             /* 7-segment display control codes for ones unit */
             case (count_detect % 10) // mod10 for units

39420dac4ac1a8da75d25b8b2ee4dfa05b4dd5c6
pcheynem1520 <pcheynem1520@gmail.com>
2020-11-30 01:28:52 -0500

    adjusted output logic equation

--- sequnce_detector.sv
+++ sequnce_detector.sv
@@ -129,7 +129,7 @@ module sequence_detector(
 
     /* output logic */
     always_comb begin
-        z <= q_ff[1] & ~q_ff[0];
+        z <= (state[1] & state[0] & sig_to_test) | (state[2] & sig_to_test);
     end
 
     /* 7-segment display control logic */
